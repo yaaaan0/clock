33359cd79cff585b369f111b3b32555bfe37d571
yaaaan0 <chun8415@gmail.com>
2020-11-16 16:03:39 +0800

    上傳

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,79 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
+const time = parseInt(process.env.VUE_APP_TIME)
+const timeBreak = parseInt(process.env.VUE_APP_TIME_BREAK)
+
 export default new Vuex.Store({
   state: {
+    alarm: 'alarm1.mp3',
+    todos: [],
+    current: '',
+    finished: [],
+    isBreak: false,
+    timeleft: time,
+    // 0 = 停止
+    // 1 = 倒數中
+    // 2 = 暫停
+    status: 0
   },
   mutations: {
+    changeStatus (state, data) {
+      state.status = data
+    },
+    selectAlarm (state, data) {
+      state.alarm = data
+    },
+    addTodo (state, data) {
+      state.todos.push(
+        {
+          name: data,
+          edit: false,
+          model: data
+        }
+      )
+    },
+    delTodo (state, index) {
+      state.todos.splice(index, 1)
+    },
+    editTodo (state, index) {
+      state.todos[index].edit = true
+    },
+    changeTodo (state, index) {
+      state.todos[index].edit = false
+      state.todos[index].name = state.todos[index].model
+    },
+    cancelTodo (state, index) {
+      state.todos[index].edit = false
+      state.todos[index].model = state.todos[index].name
+    },
+    start (state) {
+      if (state.isBreak) {
+        state.current = '休息一下'
+      } else {
+        state.current = state.todos.shift().name
+      }
+    },
+    countdown (state) {
+      state.timeleft--
+    },
+    finish (state) {
+      state.finished.push(state.current)
+      state.current = ''
+
+      if (state.todos.length > 0) {
+        state.isBreak = !state.isBreak
+      }
+
+      state.timeleft = state.isBreak ? timeBreak : time
+    }
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [createPersistedState()]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,102 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #home
+    b-container
+      b-row
+        b-col(cols="12")
+          h1 {{ currentText }}
+          h2 {{ timetext }}
+          b-btn(variant="primary" v-if="status !== 1" @click="start")
+            font-awesome-icon(:icon="['fas', 'play']")
+          b-btn(variant="primary" v-if="status === 1" @click="pause")
+            font-awesome-icon(:icon="['fas', 'pause']" )
+          b-btn(variant="primary" v-if="current.length > 0" @click="finish(true)")
+            font-awesome-icon(:icon="['fas', 'step-forward']")
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      timer: 0
+    }
+  },
+  computed: {
+    status: {
+      get () {
+        return this.$store.state.status
+      },
+      set (value) {
+        this.$store.commit('changeStatus', value)
+      }
+    },
+    current () {
+      return this.$store.state.current
+    },
+    currentText () {
+      let result = ''
+
+      if (this.current.length > 0) {
+        result = this.current
+      } else if (this.todos.length > 0) {
+        result = '點擊開始'
+      } else {
+        result = '沒有事項'
+      }
+
+      return result
+    },
+    timeleft () {
+      return this.$store.state.timeleft
+    },
+    alarm () {
+      return this.$store.state.alarm
+    },
+    todos () {
+      return this.$store.state.todos
+    },
+    timetext () {
+      const m = Math.floor(this.timeleft / 60)
+      const s = Math.floor(this.timeleft % 60)
+      return `${m} : ${s}`
+    }
+  },
+  methods: {
+    start () {
+      // 不是暫停繼續，且待辦有東西時才把 todos 的第一個變成 current
+      if (this.status !== 2 && this.todos.length > 0) {
+        this.$store.commit('start')
+      }
+      if (this.current.length > 0) {
+        this.status = 1
+        this.timer = setInterval(() => {
+          this.$store.commit('countdown')
+          if (this.timeleft <= -1) {
+            this.finish(false)
+          }
+        }, 1000)
+      }
+    },
+    finish (skip) {
+      clearInterval(this.timer)
+      this.status = 0
+      this.$store.commit('finish')
+
+      if (!skip) {
+        const audio = new Audio()
+        audio.src = './sounds' + this.alarm
+        audio.play()
+      }
+      if (this.todos.length > 0) {
+        this.start()
+      } else {
+        alert('全部結束')
+      }
+    },
+    pause () {
+      clearInterval(this.timer)
+      this.status = 2
+    }
   }
 }
 </script>
